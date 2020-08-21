92f254579174f422bfb83940a02d7c14f0f025bc
Gabriel <gab.atienza@gmail.com>
2020-08-21 16:55:39 -0700

    add error handling to yelp api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "components/SearchBar";
+import yelp from "api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: term,
+          location: "San Jose",
+        },
+      });
+      setRestaurants(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Something went wrong!");
+    }
+  };
 
   return (
     <View style={styles.SearchScreen}>
-      <SearchBar
-        onTermSubmit={() => console.log("submit!!!")}
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-      />
-      <Text>Search Screen </Text>
-      <Text>{term}</Text>
+      <SearchBar term={term} onTermSubmit={searchApi} onTermChange={setTerm} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {restaurants.length} results</Text>
     </View>
   );
 };
